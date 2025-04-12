e2b16045cfba39a194c7cb5372c623494987a6ea
kripalnathtiwari <145249601+kripalnathtiwari@users.noreply.github.com>
2025-04-12 11:24:06 +0530

    Update Day2 of Analysis.py

--- Day2 of Analysis.py
+++ Day2 of Analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+# Getting the global cumulative cases over time.
 # Load the dataset
 file_path = "C:\\Users\ASUS\Desktop\PYTHON DATASET.csv"
 df = pd.read_csv(file_path)
